879269c9ce188b21ac188d7971445f785a141c14
Faisal Memon <faisalmemonuk@yahoo.com>
2014-06-14 20:58:11 +0100

    more progress with language

--- firstPlayground.playground/section-1.swift
+++ firstPlayground.playground/section-1.swift
@@ -19,3 +19,64 @@ let named="Jonny"
 
 let announcement = "Hello \u9000 \(named) \(2.0 + 1)"
 
+var optionalString: String? = "Hello"
+optionalString == nil
+
+var optionalTree: String? = "Some Tree"
+
+var optionalName: String? = "f"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+let oak = optionalTree;
+var birch = optionalTree
+
+func tupple(name:String, specialMeal:String) -> String {
+    return "Good day \(name), you can eat \(specialMeal)"
+}
+
+tupple("tom", "Fish")
+
+func averageOf(numbers:Int...)->Int {
+    var average:Int = 0
+    var items = 0
+    for number in numbers
+    {
+        average += number
+        items   += 1
+    }
+    return average / items
+    
+}
+
+let average = averageOf(1,2,3,4)
+
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+var numbers = [20, 19, 7, 12]
+
+numbers.map({ number in 3 * number })
+
+class Shape {
+    var numberOfSides = 0
+    let dimensions = 3
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+    func split(pieces: Int) {
+        // splits shape into pieces pieces
+    }
+}
+
+var shape = Shape()
+
+
