34702029a520ad389dd327132123afab003407a9
Faisal Memon <faisalmemonuk@yahoo.com>
2014-06-14 22:42:36 +0100

    near end of intro

--- firstPlayground.playground/section-1.swift
+++ firstPlayground.playground/section-1.swift
@@ -79,4 +79,162 @@ class Shape {
 
 var shape = Shape()
 
+shape.numberOfSides=7
+var shapeDescription = shape.simpleDescription()
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Circle : NamedShape {
+    var radius : Double
+    
+    init(radius: Double, name:String)
+    {
+        self.radius = radius
+        super.init(name:name)
+    }
+    
+    func area()->Double
+    {
+        return radius * radius * 3.1415926;
+    }
+    
+    override func simpleDescription() -> String
+    {
+        return "A circle with radius \(radius)"
+    }
+}
+
+var circle = Circle(radius:3e10, name:"Big circle")
+var areaOfCircle = circle.area()
+
+var optionalCircle : Circle?
+
+optionalCircle = Circle(radius: 1e2, name: "100 Circle")
+let name = optionalCircle?.name
+
+optionalCircle = nil
+
+let otherName = optionalCircle?.name
+
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.toRaw())
+        }
+    }
+    
+    func equalTo(other:Rank) -> Bool
+    {
+        if (self.toRaw() == other.toRaw())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+let ace = Rank.Ace
+let aceRawValue = ace.toRaw()
+
+let nameOfAce:String = ace.simpleDescription()
+
+let selfcompare = ace.equalTo(ace)
+
+let dud = Rank.Two
+
+let anothercompare = ace.equalTo(dud)
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+    func color() ->String
+    {
+        switch self {
+        case .Spades, .Clubs:
+            return "black"
+        default:
+            return "red"
+        }
+    }
+}
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+let colorOfCard = hearts.color()
+
+
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+    return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+7.simpleDescription
+var a:Int = 7
+a.adjust()
+
+
+protocol AbsoluteValue {
+    var absoluteValue: Double {get}
+}
+
+extension Double:AbsoluteValue {
+    var absoluteValue: Double {
+        if (self < 0)
+        {
+            return -self;
+        }
+            else
+        {
+            return self;
+            }
+    }
+}
+
+var b:Double = -333.4
+b.absoluteValue
+
+
 
